f6709429bc40beba28d24118af2d321a46e8a23a
Leonardo Formaggio <forleo1@hotmail.com>
2025-04-04 18:46:49 +0100

    refactory pwa code

--- seviceWorker.js
+++ seviceWorker.js
@@ -14,17 +14,17 @@ const assets = [
 ];
 
 self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticMeuMercado).then(cache => {
-      cache.addAll(assets);
-    })
-  );
+    installEvent.waitUntil(
+        caches.open(staticMeuMercado).then(cache => {
+            cache.addAll(assets);
+        })
+    );
 });
 
 self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request);
-    })
-  );
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request);
+        })
+    );
 });
